4c8eea32c951e7a9b348b2219a3307f25bf37140
lynnbarakat <lynn@hellotree.co>
2023-08-09 16:07:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,83 +1,96 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [search, setSearch] = useState('');
+  const [products, setProducts] = useState([]);
+  const [productPopup, setProductPopup] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9
 
-  const [searchFields] = useState([
-    {
-      title: 'Title 1',
-      text: 'text 1'
-    },
-    {
-      title: 'Title 2',
-      text: 'text 2'
-    },
-    {
-      title: 'Title 3',
-      text: 'text 3'
-    },
-    {
-      title: 'Title 4',
-      text: 'text 4'
-    },
-  ])
+  useEffect(() => {
+    Products();
+  }, []);
 
-  // Function to highlight the search term in the given text
-  function highlightSearchTerm(text, searchTerm) {
-    if (!searchTerm) {
-      return text;
+  const Products = async () => {
+    try {
+      const response = await fetch(
+        'https://api.manoapp.com/api/v1/users/products/whats_new',
+        {
+          headers: {
+            StoreID: '4',
+            Authorization: 'f44a4aabfc5992514d262d7f517327e7',
+            UserAddressID: '60877',
+          },
+        }
+      );
+      const data = await response.json();
+      if (data.success) {
+        setProducts(data.data);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    const regex = new RegExp(searchTerm, 'gi');
-    // Replace all occurrences of the search term in the text with a highlighted version.
-    const highlightedText = text.replace(regex, match => `<span class="highlight">${match}</span>`);
-    // Return the highlighted text using dangerouslySetInnerHTML to allow rendering the HTML.
-    return <p dangerouslySetInnerHTML={{ __html: highlightedText }} />;
-  }
+  };
+
+  useEffect(() => {
+    document.querySelector('body').style.overflow = productPopup ? 'hidden' : null;
+    document.querySelector('html').style.overflow = productPopup ? 'hidden' : null;
+  }, [productPopup]);
 
   return (
-    <div className="container py-5">
-      <div className='bg-color'>
-        <form className=" p-5">
-          <div className='row'>
-            <div className="col-md-8">
-              {/* Input field for the search term */}
-              <input className="search-form w-100" type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Search..." />
-            </div>
-            <div className='col-md-12 pt-5'>
-              {/* Filtering the 'searchFields' array based on the search term to display matching results. */}
-              {
-                searchFields.filter(field =>
-                  !search ||
-                  (
-                    (field.title && field.title.toLowerCase().indexOf(search.toLowerCase()) > -1) ||
-                    (field.text && field.text.toLowerCase().indexOf(search.toLowerCase()) > -1)
-                  )
-                ).map((field, index) =>
 
-                  <div className='results' key={index}>
-                    <div className='all-results'>
-                      <div><h4>{highlightSearchTerm(field.title, search)}</h4></div>
-                      <p>{highlightSearchTerm(field.text, search)}</p>
-                    </div>
-                    <hr className='seperator' />
+    <>
+      <div className='container py-5'>
+        <div className='text-center'>
+          <h1 className='mb-5'>Products</h1>
+        </div>
+        <div className='row'>
+          {
+            products?.items
+              ?.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+              .map((product) => (
+                <div className='col-lg-4 col-sm-6 pb-4 product' onClick={() => setProductPopup(product)} key={product.id}>
+                  <div className='ratio ratio-1x1 product-img'>
+                    <img className='contain' src={product.images[0].small} alt={product.title} />
                   </div>
-                )
-              }
-              {/* // Displaying "No Results" when there are no matches based on the search term. */}
-              {
-                searchFields.filter(field =>
-                (
-                  (field.title && field.title.toLowerCase().indexOf(search.toLowerCase()) > -1) ||
-                  (field.text && field.text.toLowerCase().indexOf(search.toLowerCase()) > -1)
-                )
-                ).length === 0 && (<div>No Results</div>)}
+                  <div className='d-flex pt-5'>
+                    <h5 className='pe-3 fs flex-grow-1'>{product.title}</h5>
+                    <p>{product.price}</p>
+                  </div>
+                </div>
+              ))}
+        </div>
+      </div>
+
+      <div className='text-center mb-3'>
+        <button className=' me-2' onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>Prev</button>
+        <button className='' onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage * itemsPerPage >= products?.items?.length}>Next</button>
+      </div>
+
+      <div className={"popup " + (productPopup ? " " : " hide")}>
+        <div className='row h-100 flex-column d-flex justify-content-center align-items-center'>
+          <div className='col-lg-7 col-md-8 col-sm-10 col-10'>
+            <div className='row white-bg  position-relative'>
+              <div className='close-icon text-end' onClick={() => setProductPopup(null)}>
+                <p>x</p>
+              </div>
+              <div className='col-sm-6 col-12'>
+                <div className='ratio ratio-1x1 product-img'>
+                  <img className='contain' src={productPopup?.images[0]?.large} alt={productPopup?.title} />
+                </div>
+              </div>
+              <div className='col-sm-6 col-12 pt-sm-0 pt-4'>
+                <h5 className='pe-3 fs pb-4'>{productPopup?.title}</h5>
+                <p className='mb-0'>Price: {productPopup?.price}</p>
+                <p className='mb-0'>{productPopup?.categories[0].class_name}: {productPopup?.categories[0].category_type}</p>
+                <p>Quantity: {productPopup?.quantity}</p>
+              </div>
             </div>
           </div>
-        </form>
+        </div>
       </div>
-    </div >
+    </>
   );
 }
 
